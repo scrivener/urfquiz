1199fb0ef4a6acfc4e1c892dbaedd3b9b1e9fbac
Adam Lerner <eletari@gmail.com>
2015-04-02 17:12:09 -0400

    Very basic version of quiz interface implemented.
    Presents questions, logs answer correctness.

--- urfserver.py
+++ urfserver.py
@@ -16,7 +16,16 @@ def urfJS():
 @app.route('/questions')
 def questions():
     questions = [
-            { 'champ0': 'Annie', 'champ1': "Khz'Zix", 'correct': 'Annie', 'question': 'Who has the higher winrate in URF?'}
+            { 'champ0': 'Annie', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Zilean', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Graves', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Master Yi', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': "Rek'Sai", 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Brand', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Irelia', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Cassiopeia', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Quinn', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
+            { 'champ0': 'Twitch', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
     ]
     return json.dumps(questions)
 
