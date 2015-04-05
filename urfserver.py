921ee22b6742a51a82fa3922c8df01dc7bf5d652
Adam Lerner <eletari@gmail.com>
2015-04-04 22:57:23 -0400

    Added question generation based on data from reddit. Should replace with ours.
    The questions endpoint was also renamed to questions.json and made to have
    a content-type of application/json.

--- urfserver.py
+++ urfserver.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
 import json
+import csv
+import sys
+import random
 
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__, static_folder='static') 
+
+winrates = {}
+popularities = {}
+with open('tempdata.csv', 'r') as statsFile:
+    r = csv.DictReader(statsFile)
+    for line in r:
+        winrates[line['Champion']] = line['Win Rate']
+        popularities[line['Champion']] = line['Popularity']
 
 @app.route('/')
 def mainPage():
@@ -13,21 +24,32 @@ def mainPage():
 def urfJS():
     return app.send_static_file('urf.js')
 
-@app.route('/questions')
+@app.route('/questions.json')
 def questions():
-    questions = [
-            { 'champ0': 'Annie', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Zilean', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Graves', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Master Yi', 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': "Rek'Sai", 'champ1': "Kha'zix", 'correct': 0, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Brand', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Irelia', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Cassiopeia', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Quinn', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
-            { 'champ0': 'Twitch', 'champ1': "Kha'zix", 'correct': 1, 'question': 'Who has the higher winrate in URF?'},
-    ]
-    return json.dumps(questions)
+    questions = [generateQuestion() for x in range(10)]
+    resp = Response(response=json.dumps(questions),
+                    status=200,
+                    mimetype="application/json")
+
+    return resp
+
+def generateQuestion():
+    (champ0, champ1) = random.sample(winrates.items(), 2)
+    champ0Winrate = champ0[1]
+    champ1Winrate = champ1[1]
+    if champ0Winrate > champ1Winrate:
+        correct = 0
+    else:
+        correct = 1
+
+    return { 'champ0': champ0[0],
+             'champ1': champ1[0],
+             'champ0Winrate': champ0Winrate,
+             'champ1Winrate': champ1Winrate,
+             'correct': correct,
+             'question': 'Who has the higher winrate in URF?',
+           }
 
 if __name__ == '__main__':
+  app.debug = True
   app.run()
