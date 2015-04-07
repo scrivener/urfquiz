3ed1f0ce829ab4e8a3691a5e3abe56b741a451aa
Rachel Ruskin <rsrusk@gmail.com>
2015-04-07 10:34:30 -0400

    same as previous commit but fixing authorship

---
+++ dataframereader.py
@@ -0,0 +1,68 @@
+#to read dataframe
+
+import pickle
+import pandas as pd
+
+mydf=pickle.load(open("finaldf.p",'rb'))
+pd.set_option('display.max_columns',200)
+
+#make winlist
+
+#100_winner
+#1100championId
+#apparently ids aren't just straight from 1-124, who knew
+with open("keys.txt","rb") as keyfile:
+    keylist1=keyfile.readlines()
+keylist=[eval(item.strip()) for item in keylist1]
+print keylist
+#make a list of lists with [winnum, totalgames]
+winlist=[]
+i=0
+for champion in keylist:
+    print champion
+    winlist.append([0,0])
+    for index,row in mydf.iterrows():
+        if row['100_winner']:
+            if row['1100championId']==champion or row['2100championId']==champion \
+            or row['3100championId']==champion or row['4100championId']==champion \
+            or row['5100championId']==champion:
+                winlist[i][0]+=1
+                winlist[i][1]+=1
+            elif row['6200championId']==champion or row['7200championId']==champion \
+            or row['8200championId']==champion or row['9200championId']==champion \
+            or row['10200championId']==champion:
+                winlist[i][1]+=1
+            else:
+                pass
+        else:
+            if row['1100championId']==champion or row['2100championId']==champion \
+            or row['3100championId']==champion or row['4100championId']==champion \
+            or row['5100championId']==champion:
+                winlist[i][1]+=1
+            elif row['6200championId']==champion or row['7200championId']==champion \
+            or row['8200championId']==champion or row['9200championId']==champion \
+            or row['10200championId']==champion:
+                winlist[i][0]+=1
+                winlist[i][1]+=1
+            else:
+                pass
+    i+=1
+#tie winrates to champnames
+import json
+f=open("champion.json","rb")
+fread=f.read()
+champdict=json.loads(fread)
+champdict1=champdict[u'data']
+champnamekeydict={}
+for key in champdict1.keys():
+    champnamekeydict[eval(champdict1[key][u'key'])]=key.encode('ascii','replace')
+
+#ok so we have key: name in champnamekeydict
+#winlist [[wongames,totalgames]]
+#keylist [champid]
+from __future__ import division
+i=0
+parseablelist=[]
+for item in winlist:
+    parseablelist.append([champnamekeydict[keylist[i]], item[0]/item[1],item[1]])
+    i+=1
--- dictreader.py
+++ dictreader.py
@@ -1,51 +1,66 @@
-import simplejson
 import re
 import pandas as pd
 import ast
+import os
+import pickle
 
-with open("1780549587","rb") as f:
-    rf=f.read()
-dictf=ast.literal_eval(rf)
-dfdict={}
-#outermost level: level 1
-for key in dictf.keys():
-    entry=dictf[key]
-    if type(entry) != list:
-        dfdict[key]=entry
-    else:
-        #level 2: dict within dict
-        if len(entry) == 2:
-            #this is the dictionary of team stats for each team
-            for dict2 in entry:
-                teamId=dict2[u'teamId']
-                for key2 in dict2.keys():
-                    entry2=dict2[key2]
-                    if type(entry2) != list:
-                        idkey2=str(teamId)+'_'+key2
-                        dfdict[idkey2]=entry2
-                    else:
-                        #these are the bans
-                        #going to add to df with and without teamid
-                        for ban in entry2:                              
-                            banidkey=str(teamId)+'_'+str(ban[u'pickTurn'])+'ban'
-                            dfdict[banidkey]=ban[u'championId']
-        elif len(entry)== 10:
-            #this is the per player data.
-            #One list just contains participantids1-10; don't look at it.
-            if len(entry[0].keys()) == 1:
-                pass
-            else:
-            #the other list has ALL the player data.
+filelist=os.listdir("games/")
+all_dictionaries=[]
+for datafile in filelist:
+    urfdatapath="games/"+datafile
+    with open(urfdatapath,"rb") as f:
+        rf=f.read()
+    dictf=ast.literal_eval(rf)
+    dfdict={}
+    #outermost level: level 1
+    for key in dictf.keys():
+        entry=dictf[key]
+        if type(entry) != list:
+            dfdict[key]=entry
+        else:
+            #level 2: dict within dict
+            if len(entry) == 2:
+                #this is the dictionary of team stats for each team
                 for dict2 in entry:
+                    teamId=dict2[u'teamId']
                     for key2 in dict2.keys():
                         entry2=dict2[key2]
                         if type(entry2) != list:
-                            print key2,entry2
+                            idkey2=str(teamId)+'_'+key2
+                            dfdict[idkey2]=entry2
                         else:
-                            print('imalist')
-        else:
-            print("this shouldn't happen...")
-                            
-testdf=pd.DataFrame([dfdict])
-print testdf.head()
+                            #these are the bans
+                            #going to add to df with and without teamid
+                            for ban in entry2:                              
+                                banidkey=str(teamId)+'_'+str(ban[u'pickTurn'])+'ban'
+                                dfdict[banidkey]=ban[u'championId']
+            elif len(entry)== 10:
+                #this is the per player data.
+                #One list just contains participantids1-10; don't look at it.
+                if len(entry[0].keys()) == 1:
+                    pass
+                else:
+                #the other list has ALL the player data.
+                    for dict2 in entry:
+                        for key2 in dict2.keys():
+                            entry2=dict2[key2]
+                            if type(entry2) != list:
+                                pId=dict2[u'participantId']
+                                teamId=dict2[u'teamId']
+                                personalId=str(pId)+str(teamId)
+                                idkey=personalId+key2
+                                dfdict[idkey]=entry2
+                            else:
+                                #not dealing with this for the moment, has ind data.
+                                pass
+            else:
+                print("this shouldn't happen...")
+    all_dictionaries.append(dfdict)
+print "here is the first entry"
+print all_dictionaries[0]
+print "is there a second entry I hope"
+print all_dictionaries[1]
+finaldf=pd.DataFrame(all_dictionaries)
+pickle.dump(finaldf, open("finaldf.p","wb"))
+print finaldf.head()
             
