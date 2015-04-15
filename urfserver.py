887b3a185d970cde5bc6e0dcf45026a1bb204da1
Adam Lerner <eletari@gmail.com>
2015-04-15 13:38:59 -0400

    Better file/method comments for urfserver.py.

--- urfserver.py
+++ urfserver.py
@@ -1,5 +1,11 @@
 #!./venv/bin/python
-
+#
+# By Rachel Ruskin and Adam Lerner.
+# This server's real job is to read in winrates for champions and provide
+# the urfquiz.com/questions resource which returns a JSON list of questions
+# that the quiz should ask. It's also capable of serving the static files
+# associated with the site for testing,  but doesn't do so in the actual
+# deployed site since it's generally better to let a real webserver do that.
 import json
 import csv
 import sys
@@ -9,6 +15,7 @@ from flask import Flask, Response
 app = Flask(__name__, static_folder='static') 
 #app.debug = True
 
+# Winrates csv => dict().
 winrates = {}
 popularities = {}
 games = {}
@@ -18,17 +25,11 @@ with open('winratebychamp.csv', 'r') as statsFile:
         winrates[line['champion']] = line['winrate']
         games[line['champion']] = line['games']
 
-# These static routes are never used in deployment (nginx serves the static
-# files directly and only routes /questions to this app.
-# We leave them in for ease of debugging.
-@app.route('/')
-def mainPage():
-    return app.send_static_file('index.html')
-@app.route('/<path>')
-def urfStatic(path):
-    return app.send_static_file(path)
-
-
+# Endpoint that returns a boatload of questions. We punt and return 500,
+# since we found in testing that many users like to just click as fast as
+# they can on one of the buttons and hope they get a high score, which honestly
+# is a reasonable strategy in URF mode, which is all about action and fun, so
+# we accomodate them.
 @app.route('/questions')
 def questions():
     questions = [generateQuestion() for x in range(500)]
@@ -38,6 +39,7 @@ def questions():
 
     return resp
 
+# Generates a single question consisting of two champions, with their winrates.
 def generateQuestion():
     (champ0, champ1) = random.sample(winrates.items(), 2)
     champ0Winrate = champ0[1]
@@ -54,6 +56,16 @@ def generateQuestion():
              'correct': correct,
              'question': 'Who has the higher winrate in URF?',
            }
+
+# These static routes are never used in deployment (nginx serves the static
+# files directly and only routes /questions to this app.
+# We leave them in for ease of debugging.
+@app.route('/')
+def mainPage():
+    return app.send_static_file('index.html')
+@app.route('/<path>')
+def urfStatic(path):
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
